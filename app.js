cc59475efbb74e2ec2c39fd3667f37f070b871ab
dev-04-spec <kalemanoj3@gmail.com>
2021-02-20 20:48:41 +0530

    all commit using vs code

--- app.js
+++ app.js
@@ -1,5 +1,63 @@
-const fs=require('fs');
-for(let i=0;i<10;i++){
-    console.log(i);
-}
-fs.writeFileSync('notes.txt','This file was created by Node.js');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler() {
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse()
